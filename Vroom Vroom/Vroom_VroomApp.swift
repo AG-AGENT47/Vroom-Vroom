556de3178d62c907e779a864bd0646661beb3b29
Avyakt Garg <garg62@wisc.edi>
2025-12-29 22:29:39 -0500

    Initial Commit

---
+++ Vroom Vroom/Vroom_VroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vroom_VroomApp.swift
+//  Vroom Vroom
+//
+//  Created by Avyakt Garg on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Vroom_VroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vroom VroomTests/Vroom_VroomTests.swift
@@ -0,0 +1,17 @@
+//
+//  Vroom_VroomTests.swift
+//  Vroom VroomTests
+//
+//  Created by Avyakt Garg on 29/12/25.
+//
+
+import Testing
+@testable import Vroom_Vroom
+
+struct Vroom_VroomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
